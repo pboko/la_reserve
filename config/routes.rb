2971b4a325712367f563110653c8a64edb6d16c4
nathan obadia <natobadia@gmail.com>
2015-06-30 15:23:54 +0200

    premiers changements booking controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'bookings/index'
+
+  get 'bookings/show'
+
+  get 'bookings/new'
+
+  get 'bookings/create'
+
+  get 'bookings/edit'
+
+  get 'bookings/update'
+
+  get 'bookings/destroy'
+
   devise_for :users
 end
