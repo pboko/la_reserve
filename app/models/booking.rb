5aac467347f0b8807eef5da18a773207795bf9c4
nathan obadia <natobadia@gmail.com>
2015-06-30 15:00:03 +0200

    create structure

---
+++ app/models/booking.rb
@@ -0,0 +1,29 @@
+# == Schema Information
+#
+# Table name: bookings
+#
+#  id            :integer          not null, primary key
+#  start_time    :time
+#  end_time      :time
+#  date          :date
+#  pax           :integer
+#  restaurant_id :integer
+#  customer_id   :integer
+#  waiter_id     :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+# Indexes
+#
+#  index_bookings_on_customer_id    (customer_id)
+#  index_bookings_on_restaurant_id  (restaurant_id)
+#  index_bookings_on_waiter_id      (waiter_id)
+#
+
+class Booking < ActiveRecord::Base
+  belongs_to :restaurant
+  belongs_to :customer
+  belongs_to :waiter
+  has_many :booking_spots
+  has_many :spots, through: :booking_spots
+end
