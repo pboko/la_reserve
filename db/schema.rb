e6675f462159b605686076b22bb7ca08e14e1366
nathan obadia <natobadia@gmail.com>
2015-07-06 17:51:04 +0200

    page customer brillamment dynamisée

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150704152721) do
+ActiveRecord::Schema.define(version: 20150706144308) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,8 +85,12 @@ ActiveRecord::Schema.define(version: 20150704152721) do
     t.string   "street"
     t.string   "zip_code"
     t.integer  "capacity"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+    t.string   "picture_file_name"
+    t.string   "picture_content_type"
+    t.integer  "picture_file_size"
+    t.datetime "picture_updated_at"
   end
 
   create_table "spots", force: :cascade do |t|
