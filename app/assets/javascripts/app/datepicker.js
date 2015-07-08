a3cbe9da323dde7e89fab0851ab0954bf6a68bbb
Karine Bialobroda <karine.bialobroda@gmail.com>
2015-07-08 18:59:33 +0200

    test

--- app/assets/javascripts/app/datepicker.js
+++ app/assets/javascripts/app/datepicker.js
@@ -4,10 +4,15 @@ $(document).ready(function(){
     defaultViewDate: new Date()
   });
 
-  $('#btn-resa').on('click', function(){
-    $('.datepicker').datepicker({
-      format: "yyyy-mm-dd",
-      defaultViewDate: new Date()
-    });
-  })
+
+  // $('body').on('click', '.datepicker td', function(){
+  //   $('.datepicker').remove()
+  // })
+
+  // $('#btn-resa').on('click', function(){
+  //   $('.datepicker').datepicker({
+  //     format: "yyyy-mm-dd",
+  //     defaultViewDate: new Date()
+  //   });
+  // })
 });
