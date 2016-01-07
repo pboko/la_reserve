d0cb35f01645405480052b2ca777e326b1091dac
Olivia Brami <olivia.brami@gmail.com>
2016-01-07 12:02:14 +0100

    remove www on prod url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp { host: "www.la-reserve.herokuapp.com", locale: I18n.locale }
+  config.action_mailer.delivery_method = :smtp { host: "la-reserve.herokuapp.com", locale: I18n.locale }
 
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
